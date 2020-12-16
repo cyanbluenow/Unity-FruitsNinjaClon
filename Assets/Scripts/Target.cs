d3cbc5ca70760ac5df76fdd1e30cd6b883514d2b
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-16 10:33:57 +0100

    add prefabs functions

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float
+        minForce = 12,
+        maxForce = 16,
+        maxTorque = 10,
+        xRange= 4,
+        ySpawnPos = -6;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+}
